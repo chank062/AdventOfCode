4b71165f6597e8b3b831425c3733a5483525d6c4
Kelsey Chan <kelseychan@PRA-0011.local>
2018-12-02 22:21:23 +0000

    day 1 part 2

--- day_1/thing.py
+++ day_1/thing.py
@@ -6,4 +6,18 @@ count = 0
 for line in lines:
     count += int(line)
 
-print("Part 1: " + str(count))
+print("Part 1: " + str(count)) #505
+
+count2 = set()
+count = 0
+pos = 0
+
+while (True):
+    # [pos %remainder (number of lines)]
+    count += int(lines[pos % len(lines)])
+    if (count in count2):
+        break # if value is in set (then you've found the reocurring frequency)
+    count2.add(count) # add to set
+    pos += 1 # increment
+
+print("Part 2: " + str(count)) # 72330
